1ac7bbf6d9cf3d277bfd11471ec3c26f61d01fae
MapleleavesCX <129497023+MapleleavesCX@users.noreply.github.com>
2024-09-07 16:59:58 +0800

    Update DecisionTree.py

--- ml/model/DecisionTree.py
+++ ml/model/DecisionTree.py
@@ -24,18 +24,18 @@ class SecureDecisionTree:
     def __init__(self, spu):
         self.Xgb = Xgb(spu)
 
-    def fit(self, X_train, y_train, params={
-            'num_boost_round': 5,
-            'max_depth': 5,
-            'learning_rate': 0.1,
-            'sketch_eps': 0.08,
-            'objective': 'logistic',
-            'reg_lambda': 0.1,
-            'subsample': 1,
-            'colsample_by_tree': 1,
-            'base_score': 0.5,
-        }):
-        self.model=self.Xgb.train(dtrain=X_train, label=y_train, **params)
+    def fit(self, X_train, y_train, params = {
+	    'num_boost_round': 5,
+	    'max_depth': 5,
+	    'learning_rate': 0.1,
+	    'sketch_eps': 0.08,
+	    'objective': 'logistic',
+	    'reg_lambda': 0.1,
+	    'subsample': 1,
+	    'colsample_by_tree': 1,
+	    'base_score': 0.5,
+	}):
+        self.model=self.Xgb.train(dtrain=X_train, label=y_train, params=params)
 
     def predict(self, X_test):
         return self.model.predict(X_test)
