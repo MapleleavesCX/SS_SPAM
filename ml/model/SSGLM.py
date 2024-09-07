7a098594b04baecc9cf0a919607e04b710ecf73a
MapleleavesCX <129497023+MapleleavesCX@users.noreply.github.com>
2024-09-07 17:00:43 +0800

    Update SSGLM.py

--- ml/model/SSGLM.py
+++ ml/model/SSGLM.py
@@ -2,15 +2,15 @@
 # @Date    : 2024/08/25
 # @Author  : Taorich
 
-
+import numpy as np
 from secretflow.ml.linear.ss_glm import SSGLM
 
 # train训练额外参数参考列表：
 '''
+
 params = {
-    'learning_rate': 0.1,  # 可选参数，默认值，控制每轮迭代中模型更新的程度
-    'batch_size': 1024,    # 可选参数，默认值，每轮计算使用的样本数量
-    'eps': 1e-4,           # 可选参数，默认值，用于判断收敛的标准
+    'offset':None,         # 可选参数，指定一个列作为偏移量使用
+    'weight':None,         # 可选参数，指定一个列来用作观测权重
     'epochs': 100,         # 可选参数，迭代轮数，可以根据实际情况调整
     'l2_lambda': 0.01      # 可选参数， L2 正则化系数，可以适当调整以防止过拟合
 }
@@ -21,24 +21,25 @@ class SecureSSGLM:
     def __init__(self, spu):
         self.model = SSGLM(spu)
 
-    def fit(self, X_train, y_train, option='sgd', params = {
-                    'learning_rate': 0.1,
-                    'batch_size': 1024,
-                    'eps': 1e-4,
+    def fit(self, X_train, y_train, option='sgd', sgd_LR=0.1, sgd_BatchSize=1024, params = {
+                    'offset':None,
+                    'weight':None,
                     'epochs': 100,
                     'l2_lambda': 0.01
                 }):
-        
+        # 根据官方文档：
+        # https://www.secretflow.org.cn/zh-CN/docs/secretflow/v1.5.0b0/source/secretflow.ml.linear.ml.linear.ss_glm#secretflow.ml.linear.ss_glm.SSGLM.fit_irls
+        # 只有 fit_sgd 有参数 learning_rate 和 batch_size ，故调整如下：
         if option == 'sgd':
-            self.model.fit_sgd(X_train, y_train, **params)
+            self.model.fit_sgd(X_train, y_train, link='Logit', dist='Bernoulli', 
+                               learning_rate=sgd_LR, batch_size=sgd_BatchSize **params)
         elif option == 'irls':
-            self.model.fit_irls(X_train, y_train, **params)
+            self.model.fit_irls(X_train, y_train, link='Logit', dist='Bernoulli', **params)
         else:
             raise ValueError(f"No mode found for '{option}'")
         
 
     def predict(self, X_test):
         return self.model.predict(X_test)
-
 
 
